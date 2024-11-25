2a220125bb8dde56f381016ba5a3388ed88c1c6f
Victor Ojeda <victor.ojeda@bethedriver.global>
2024-11-25 00:27:46 -0300

    Frontend

--- frontend/src/app/predicciones/predicciones.component.ts
+++ frontend/src/app/predicciones/predicciones.component.ts
@@ -7,9 +7,9 @@ import { Component } from '@angular/core';
 })
 export class PrediccionesComponent {
   cultivos = [
-    { nombre: 'Maíz', porcentaje: 92, 'temperatura': '12°', 'humedad': '7%', 'lluvia': '4mm', riego: 'Moderado', tiempoCosecha: 120 },
-    { nombre: 'Trigo', porcentaje: 85, 'temperatura': '22°', 'humedad': '3%', 'lluvia': '30mm', riego: 'Bajo', tiempoCosecha: 110 },
-    { nombre: 'Arroz', porcentaje: 78, 'temperatura': '33°', 'humedad': '1%', 'lluvia': '12mm', riego: 'Alto', tiempoCosecha: 150 },
+    { nombre: 'Predicción 1', porcentaje: 92, 'temperatura': '12°', 'humedad': '7%', 'lluvia': '4mm', riego: 'Moderado', tiempoCosecha: 120 },
+    { nombre: 'Predicción 2', porcentaje: 85, 'temperatura': '22°', 'humedad': '3%', 'lluvia': '30mm', riego: 'Bajo', tiempoCosecha: 110 },
+    { nombre: 'Predicción 3', porcentaje: 78, 'temperatura': '33°', 'humedad': '1%', 'lluvia': '12mm', riego: 'Alto', tiempoCosecha: 150 },
     // Agrega más cultivos según sea necesario
   ];
 }
